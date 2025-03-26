9b0dd970b7a6bd1f9e38bf02b8e70843ec4357ee
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-03-26 17:11:38 +0100

    Update Mission.tsx
    updated the Lorem text

--- components/Mission.tsx
+++ components/Mission.tsx
@@ -11,12 +11,20 @@ const Mission = () => {
      <div className='my-5' >
       <div className='text-2xl text-white bg-orange-700 rounded-full w-fit p-3 text-center' ><FaSuitcase/></div>
       <div className='text-xl my-4 font-bold' >Helping talents gain experience through real tasks</div>
-      <div className='text-sm  min-[640px]:my-4 max-md:w-full w-[80%] ' >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur quibusdam eaque quidem, provident sequi earum, cumque dolore aperiam impedit deleniti quo? Porro perspiciatis quaerat corporis natus. Doloribus quo maxime deleniti?</div>
+      <div className='text-sm  min-[640px]:my-4 max-md:w-full w-[80%] ' >At Skelv, we bridge the gap between learning and real-world experience.
+ Our platform empowers aspiring professionals by connecting them with real tasks from small businesses, allowing them to 
+build skills, gain confidence, and create a portfolio that speaks for itself. By working on practical projects, talents 
+transform their knowledge into hands-on expertise, setting the foundation for future career success.
+</div>
      </div> 
      <div className='' >
       <div className='text-2xl text-white bg-orange-700 rounded-full w-fit p-3 text-center' ><MdGroups/></div>
-      <div className='text-xl my-4 font-bold' >Helping talents gain experience through real tasks</div>
-      <div className='text-sm min-[640px]:my-4 max-md:w-full w-[80%] ' >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur quibusdam eaque quidem, provident sequi earum, cumque dolore aperiam impedit deleniti quo? Porro perspiciatis quaerat corporis natus. Doloribus quo maxime deleniti?</div>
+      <div className='text-xl my-4 font-bold' >Helping Small Businesses Grow with Skilled Support</div>
+      <div className='text-sm min-[640px]:my-4 max-md:w-full w-[80%] ' >Skelv is dedicated to assisting small-scale businesses grow by giving 
+them access to skilled talents eager to contribute. Our platform offers cost-effective solutions, enabling businesses to 
+get quality work done while supporting aspiring professionals in gaining real-world experience. By bridging this gap, we
+ create a win-win environment where businesses thrive, and talents sharpen their skills.
+</div>
      </div>
      
      </div>
