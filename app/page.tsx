66a86e0de4894900ee4f2ec189c02036ef7b92d6
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-03-26 07:37:55 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import {BackToTop, Banner, Faq, Footer, Header, Hero, HowItWorks, Mission, Vision } from '@/components'
-=======
-import {Faq, Footer, Header, Hero, Mission, Vision } from '@/components'
->>>>>>> f52cc9f (first commit)
-=======
-import {BackToTop, Banner, Faq, Footer, Header, Hero, HowItWorks, Mission, Vision } from '@/components'
->>>>>>> 782717e (completed with waitlist)
+
 import React from 'react'
 
 const Home = () => {
   return (
     <>
-<<<<<<< HEAD
-<<<<<<< HEAD
       <main>
         <BackToTop/>
         <Hero/>
@@ -22,24 +14,6 @@ const Home = () => {
         <HowItWorks/>
         <Faq/>
         <Banner/>
-=======
-      <header >
-        <Header/>
-      </header>
-=======
->>>>>>> 782717e (completed with waitlist)
-      <main>
-        <BackToTop/>
-        <Hero/>
-        <Mission/>
-        <Vision/>
-        <HowItWorks/>
-        <Faq/>
-<<<<<<< HEAD
->>>>>>> f52cc9f (first commit)
-=======
-        <Banner/>
->>>>>>> 782717e (completed with waitlist)
       </main>
       <footer>
         <Footer/>
