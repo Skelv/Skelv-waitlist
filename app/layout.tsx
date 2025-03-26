8e7182012cfcceb52893ea4743c0daa9639bf72b
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-03-26 07:33:10 +0100

    Fixed layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { Header } from "@/components";
 
 
@@ -12,24 +10,7 @@ export const metadata = {
   other: {
     "stylesheet":"https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.css",
   }
-=======
-=======
-import { Header } from "@/components";
->>>>>>> 782717e (completed with waitlist)
 
-
-export const metadata = {
-  title: "Skelv",
-  description: "Get Hired, Gain Experience, Build Your Portfolio.",
-  icons: 'globe.svg',
-<<<<<<< HEAD
->>>>>>> f52cc9f (first commit)
-=======
-  other: {
-    "stylesheet":"https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.css",
-  }
->>>>>>> 782717e (completed with waitlist)
-};
 
 export default function RootLayout({
   children,
@@ -41,14 +22,8 @@ export default function RootLayout({
       <body
         className=""
       >
-<<<<<<< HEAD
-<<<<<<< HEAD
         <Header/>
-=======
->>>>>>> f52cc9f (first commit)
-=======
-        <Header/>
->>>>>>> 782717e (completed with waitlist)
+
         {children}
       </body>
     </html>
