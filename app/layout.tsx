211b6f88f4d31a097c420158e9c8114f2a342254
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-03-27 09:17:30 +0100

    Update layout.tsx
    Added icon metatag

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,12 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Header } from "@/components";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Skelv",
   description: "Get Hired, Gain Experience, Build Your Portfolio.",
-  icons: 'globe.svg',
+  icons: {
+    icon: "Skelv logo.png",
+  },
   other: {
     stylesheet: "https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.css",
   },
